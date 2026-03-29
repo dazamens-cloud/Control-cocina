71d98b56c3d2ac03bd81796e8fabe03cb7e3b792
dazamens-cloud <dazamens@gmail.com>
2026-03-29 23:22:43 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'divina-italia-v2';
+const CACHE_NAME = 'divina-italia-v3';
 const ASSETS = [
   '/Control-cocina/',
   '/Control-cocina/index.html',
@@ -8,9 +8,26 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting(); // activa el nuevo SW inmediatamente
+});
+
+self.addEventListener('activate', e => {
+  // elimina cachés viejas
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim(); // toma control de todas las pestañas abiertas
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
