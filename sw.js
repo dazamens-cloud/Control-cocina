91061848fc471ce3413011e9eeb2cf6891bd9533
dazamens-cloud <dazamens@gmail.com>
2026-03-29 00:54:09 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,16 @@
-const CACHE_NAME = 'divina-italia-cache-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './style.css',
-  './script.js',
-  './manifest.json'
+const CACHE_NAME = 'divina-italia-v2';
+const ASSETS = [
+  '/Control-cocina/',
+  '/Control-cocina/index.html',
+  '/Control-cocina/style.css',
+  '/Control-cocina/script.js',
+  '/Control-cocina/manifest.json'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
