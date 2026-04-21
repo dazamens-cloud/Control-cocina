0d2bd80b6fe702c3644aad9494da5f47bf08887f
dazamens-cloud <dazamens@gmail.com>
2026-04-21 23:29:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // v3.1 — Bugs de sintaxis corregidos
 // =============================================
 
-const URL_SCRIPT    = "https://script.google.com/macros/s/AKfycbybrFFcwNOSn7X1r4lSt0VMAADBbLtkdOr4EFS5iTrN4ByA8zW8hqoBSIb3LMDtC5pzkA/exec";
+const URL_SCRIPT    = "https://script.google.com/macros/s/AKfycbxrkgWa4BFnPwU6VWPSoUEmk4-IRbyJ27JyoUj1Cs-4LCzjY1ESuzAUHYZMNZOqzVtHow/exec";
 const WEB_APP_TOKEN = "DivinaItalia2026#Charco";
 
 // ── ESTADO GLOBAL ───────────────────────────
